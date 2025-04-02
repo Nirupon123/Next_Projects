be2ccd48a3b8f8a210b97ddb67b9c18a29233752
Nirupon123 <niruponpal@gmail.com>
2025-04-03 01:20:33 +0530

    ..

---
+++ myfirst_nextapp/app/signin/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Signin() { //file based routing
+    return (
+      <div>
+        <h1>sign in</h1>
+      </div>
+    );
+  }
---
+++ myfirst_nextapp/app/signup/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Signup() { //file based routing
+    return (
+      <div>
+        <h1>sign up</h1>
+      </div>
+    );
+  }
